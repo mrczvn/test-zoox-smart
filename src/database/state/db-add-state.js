81d926c61eca8e0db4e8d187e7693fe28d740d88
Marcos Vinicius <marvn.ants1@gmail.com>
2020-10-08 00:22:01 -0300

    test: ensure test coverage in DbAddState

---
+++ src/database/state/db-add-state.js
@@ -0,0 +1,19 @@
+const dbAddState = (loadStateByNameRepository, addStateRepository) => ({
+  async add({ nome, abreviacao }) {
+    const state = await loadStateByNameRepository.loadByName(nome)
+
+    if (!state) {
+      const atThisMoment = new Date()
+
+      return await addStateRepository.add({
+        nome,
+        abreviacao,
+        data_criacao: atThisMoment,
+        data_da_ultima_alteracao: atThisMoment
+      })
+    }
+    return null
+  }
+})
+
+module.exports = dbAddState
---
+++ src/database/state/db-add-state.spec.js
@@ -0,0 +1,72 @@
+const dbAddState = require('./db-add-state')
+const {
+  AddStateRepositorySpy,
+  LoadStateByNameRepositorySpy,
+  mockAddStateParams,
+  mockStateModel
+} = require('../../test')
+
+const throwError = () => {
+  throw new Error()
+}
+
+const makeSut = () => {
+  const addStateRepositorySpy = AddStateRepositorySpy()
+  const loadStateByNameRepositorySpy = LoadStateByNameRepositorySpy()
+
+  const sut = dbAddState(loadStateByNameRepositorySpy, addStateRepositorySpy)
+
+  return { sut, loadStateByNameRepositorySpy, addStateRepositorySpy }
+}
+
+describe('DbAddState', () => {
+  const { nome, abreviacao } = mockAddStateParams()
+
+  test('Should call LoadStateByNameRepositorySpy with correct values', async () => {
+    const { sut, loadStateByNameRepositorySpy } = makeSut()
+
+    await sut.add({ nome, abreviacao })
+
+    expect(loadStateByNameRepositorySpy.name).toBe(nome)
+  })
+
+  test('Should return null if LoadStateByNameRepositorySpy returns an null', async () => {
+    const { sut, loadStateByNameRepositorySpy } = makeSut()
+
+    loadStateByNameRepositorySpy.state = mockStateModel()
+
+    const stateData = await sut.add({ nome, abreviacao })
+
+    expect(stateData).toBeNull()
+  })
+
+  test('Should throw if LoadStateByNameRepositorySpy throws', async () => {
+    const { sut, loadStateByNameRepositorySpy } = makeSut()
+
+    jest
+      .spyOn(loadStateByNameRepositorySpy, 'loadByName')
+      .mockImplementationOnce(throwError)
+
+    const stateData = sut.add({ nome, abreviacao })
+
+    await expect(stateData).rejects.toThrow()
+  })
+
+  test('Should call AddStateRepositorySpy with correct values', async () => {
+    const { sut, addStateRepositorySpy } = makeSut()
+
+    await sut.add({ nome, abreviacao })
+    expect(addStateRepositorySpy.addStateParams.nome).toBe(nome)
+    expect(addStateRepositorySpy.addStateParams.abreviacao).toBe(abreviacao)
+  })
+
+  test('Should throw if AddStateRepositorySpy throws', async () => {
+    const { sut, addStateRepositorySpy } = makeSut()
+
+    jest.spyOn(addStateRepositorySpy, 'add').mockImplementationOnce(throwError)
+
+    const stateData = sut.add({ nome, abreviacao })
+
+    await expect(stateData).rejects.toThrow()
+  })
+})
